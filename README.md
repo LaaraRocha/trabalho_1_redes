1277aca89bcbcc2df568f4c2e5f9429512a1f641
Laara <laara.rocha@unidavi.edu.br>
2022-03-24 20:22:10 -0300

    quarto

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # trabalho_1_redes
+Utilizar PHP Storm ou VSCode
+No terminal digitar "cd .\backend\" e dar enter
+<br>
+No terminal digitar ".node .\servidor.js e dar enter
--- frontend/index.php
+++ frontend/index.php
@@ -20,27 +20,37 @@ $retorno = '';
 <head>
     <meta charset="UTF-8">
     <title>Chamada http</title>
+    <link rel="stylesheet" type="text/css" href="./resources/style.css" media="screen" />
 </head>
     <body>
 
         <form method="post">
+            <fieldset class="msgcliente">
             <div class="cliente">
                 <legend><strong>Digite sua mensagem</strong></legend>
+                <br>
                 <label for=clientela"">Origem</label>
+                <br>
                 <input type="text" id="clientela" name="client" required="required" placeholder="Mensagem do cliente">
 
                 <input type="submit" id="enviar" name="enviar_mensagem" value="Envia">
             </div>
+            </fieldset>
 
+            <fieldset class="msg">
             <div class="mensagem">
                 <pre><?
                         echo $retorno;
                     ?></pre>
             </div>
+            </fieldset>
 
+            <fieldset class="serv">
             <div class="servidor">
+                <p>Resposta do Server</p>
                 <pre></pre>
             </div>
+            </fieldset>
         </form>
 
     </body>
